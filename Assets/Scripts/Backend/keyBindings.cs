94370a69f01aa18250f049b7b95155a85c44cb81
Nicholas <niwith@gmail.com>
2015-12-15 18:29:53 +1000

    first commit

---
+++ Assets/Scripts/Backend/keyBindings.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class keyBindings : MonoBehaviour {
+
+    public string camForward = "w";
+    public string camRight = "d";
+    public string camLeft = "a";
+    public string camBack = "s";
+    public string camUp = "=";
+    public string camDown = "-";
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Scripts/Camera/cameraHeightControl.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class cameraHeightControl : MonoBehaviour {
+
+    public float cameraHeight = 20;
+    public float difference = 0;
+    public float cameraAjustSpeed = 10;
+    public float minHeight = 20;
+    public float maxHeight = 200;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        cameraHeight += Input.GetAxis("Mouse ScrollWheel")*Time.deltaTime*cameraAjustSpeed*-1000;
+        
+        if(cameraHeight < minHeight)
+        {
+            cameraHeight = minHeight;
+        }
+        else if(cameraHeight > maxHeight)
+        {
+            cameraHeight = maxHeight;
+        }
+
+        RaycastHit hit;
+        Ray downRay = new Ray(transform.position, -Vector3.up);
+        if (Physics.Raycast(downRay, out hit))
+        {
+            difference = cameraHeight - hit.distance;
+        }
+
+        float newHeight = transform.position.y + difference;
+        transform.position = new Vector3(transform.position.x, newHeight, transform.position.z);
+    }
+}
